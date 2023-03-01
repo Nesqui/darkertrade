08428e8140d5dbe2804a628ba9ae129ce9359bd7
Xloctis <pm@git.hub>
2023-03-01 20:30:45 +0300

    LF eol enforce

--- backend/src/existing-item/existing-item.service.ts
+++ backend/src/existing-item/existing-item.service.ts
@@ -25,7 +25,7 @@ export class ExistingItemService {
     private attributePairsRepository: typeof AttributePair,
     @Inject('ATTRIBUTES_REPOSITORY')
     private attributeRepository: typeof Attribute,
-  ) { }
+  ) {}
 
   async create(createExistingItemDto: CreateExistingItemDto, user: User) {
     const item = await this.existingItemRepository.create(
@@ -91,31 +91,36 @@ export class ExistingItemService {
       }
     }
 
-
     // separate slots
     // gunдонскuй methoд
-    const itemIdLF = [1, 2, 3, 4, 5].includes(baseItem.item.id) ? [1, 2, 3, 4, 5] : [6, 7, 8, 9].includes(baseItem.item.id) ? [6, 7, 8, 9] : [baseItem.item.id]
-    const whereItemRep = { id: itemIdLF }
+    const itemIdLF = [1, 2, 3, 4, 5].includes(baseItem.item.id)
+      ? [1, 2, 3, 4, 5]
+      : [6, 7, 8, 9].includes(baseItem.item.id)
+      ? [6, 7, 8, 9]
+      : [baseItem.item.id];
+    const whereItemRep = { id: itemIdLF };
 
-
-    const baseItemStatsArr = baseItem.stats.map(a => a.attributeId)
+    const baseItemStatsArr = baseItem.stats.map((a) => a.attributeId);
     const pairedAttributes = await this.attributePairsRepository.findAll({
       where: {
-        attributeId: baseItemStatsArr
-      }
-    })
+        attributeId: baseItemStatsArr,
+      },
+    });
     //pv cver appendnut array
-    const similarStats = baseItem.stats.map(a => a.attributeId)
+    const similarStats = baseItem.stats.map((a) => a.attributeId);
     for (const pair of pairedAttributes) {
-      similarStats.push(pair.destAttributeId)
+      similarStats.push(pair.destAttributeId);
     }
     // soglasovano na match
-    const amountOfStats = baseItem.stats.length > 2 ? baseItem.stats.length - 1 : baseItem.stats.length;
-    const statsLF = [...chooseKCombos(similarStats, amountOfStats)]
+    const amountOfStats =
+      baseItem.stats.length > 2
+        ? baseItem.stats.length - 1
+        : baseItem.stats.length;
+    const statsLF = [...chooseKCombos(similarStats, amountOfStats)];
 
     const whereStatRep = {
-      [sequelize.Op.or]: statsLF.map(a => ({ attributeId: a }))
-    }
+      [sequelize.Op.or]: statsLF.map((a) => ({ attributeId: a })),
+    };
 
     const itemList = await this.existingItemRepository.findAll({
       where: {
@@ -124,40 +129,38 @@ export class ExistingItemService {
       include: [
         {
           model: this.statRepository,
-          where: whereStatRep
-        }
-        ,
+          where: whereStatRep,
+        },
         {
           model: this.itemRepository,
-          where: whereItemRep
+          where: whereItemRep,
         },
-        this.userRepository],
+        this.userRepository,
+      ],
     });
 
-    //logic   
-    const answer = []
-    const shadowArr = []
-    let valueOfStats = 0
+    //logic
+    const answer = [];
+    const shadowArr = [];
+    let valueOfStats = 0;
     for (const item of itemList) {
-
-      valueOfStats = 0
+      valueOfStats = 0;
       for (const stat of item.stats) {
-        if (baseItemStatsArr.includes(stat.attributeId)) valueOfStats += Number(stat.value)
-        valueOfStats += Number(stat.value) * 2
+        if (baseItemStatsArr.includes(stat.attributeId))
+          valueOfStats += Number(stat.value);
+        valueOfStats += Number(stat.value) * 2;
       }
-      shadowArr.push(valueOfStats)
+      shadowArr.push(valueOfStats);
     }
 
-    let copy = 0
+    let copy = 0;
     for (let i = 0; i < shadowArr.length; i++) {
-      copy = shadowArr.indexOf(Math.max(...shadowArr))
-      shadowArr[i] = -30
-      answer.push(itemList[copy])
+      copy = shadowArr.indexOf(Math.max(...shadowArr));
+      shadowArr[i] = -30;
+      answer.push(itemList[copy]);
     }
 
-
-    return answer
-
+    return answer;
   }
 
   update(id: number, updateExistingItemDto: UpdateExistingItemDto) {
