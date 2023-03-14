f92960635a60bf88ad5cc15002fde1d9944bfc4f
Xloctis <pm@git.hub>
2023-03-14 22:18:45 +0300

    fix eco test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   apps: [{
-    name: 'be',
-    cwd: '/opt/darkertrade/backend/dist/',
-    "script": "pnpm",
-    "args": "run start",
+    name: "be",
+    cwd: "/opt/darkertrade/backend/dist/",
+    script: "pnpm",
+    args: "run start",
     instances: 1,
     autorestart: true,
     watch: false,
-    error_file: '/opt/darkertrade/logs/backend-err.log',
-    out_file: '/opt/darkertrade/logs/backend-out.log'
+    error_file: "/opt/darkertrade/logs/backend-err.log",
+    out_file: "/opt/darkertrade/logs/backend-out.log"
   },
   {
-    name: 'fe',
-    cwd: '/opt/darkertrade/frontend/',
-    "script": "pnpm",
-    "args": "run build",
+    name: "fe",
+    cwd: "/opt/darkertrade/frontend/",
+    script: "pnpm",
+    args: "run build",
     instances: 1,
-    error_file: '/opt/darkertrade/logs/frontend-err.log',
-    out_file: '/opt/darkertrade/logs/frontend-out.log'
+    error_file: "/opt/darkertrade/logs/frontend-err.log",
+    out_file: "/opt/darkertrade/logs/frontend-out.log"
   }]
 }
